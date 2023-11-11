99d15c806dd35679a1f285371b7e24fe86e990bb
Vladyslav Teteruk <teteryk.vladislav@gmail.com>
2023-11-11 20:06:38 +0200

    fnsh

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,6 +13,18 @@ class App extends Component {
     ],
     filter: ''
   }
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    if (contacts) {
+      this.setState({ contacts: JSON.parse(contacts) });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   handleAddContact = contact => {
     this.setState(prevState => ({
